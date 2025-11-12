ee1388f60cfe242fb8b527dda5df4d3a922825f7
weflane <weflane@gmail.com>
2025-11-12 15:52:33 +0300

    дз

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -4,5 +4,20 @@ package ru.tbank.education.school.lesson1
  * Сумма четных чисел.
  */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var sum = 0
+    for (n in numbers) {
+        if (n % 2 == 0) {
+            sum += n
+        }
+    }
+    return sum
+}
+
+fun main() {
+    val a = arrayOf(2, 4)
+    val x = arrayOf(1, 3, 5)
+    val y = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(sumEvenNumbers(a))
+    println(sumEvenNumbers(x))
+    println(sumEvenNumbers(y))
 }
