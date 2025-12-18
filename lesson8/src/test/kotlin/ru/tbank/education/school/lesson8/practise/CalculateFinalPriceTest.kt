5f27e21cedf9b49f08a8ed8efdd25d4bfe47e5f1
weflane <weflane@gmail.com>
2025-12-18 11:48:55 +0300

    *

--- lesson8/src/test/kotlin/ru/tbank/education/school/lesson8/practise/CalculateFinalPriceTest.kt
+++ lesson8/src/test/kotlin/ru/tbank/education/school/lesson8/practise/CalculateFinalPriceTest.kt
@@ -1,7 +1,10 @@
 package ru.tbank.education.school.lesson8.practise
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
 /**
- *
  * Сценарии для тестирования:
  *
  * 1. Позитивные сценарии (happy path):
@@ -15,3 +18,48 @@ package ru.tbank.education.school.lesson8.practise
  *    - Скидка вне диапазона: discountPercent < 0 или > 100 → IllegalArgumentException.
  *    - Налог вне диапазона: taxPercent < 0 или > 30 → IllegalArgumentException.
  */
+
+class PriceCalculatorTest {
+
+    fun calculateFinalPrice(
+        basePrice: Double,
+        discountPercent: Int,
+        taxPercent: Int
+    ): Double {
+        require(value = basePrice >= 0) { "Base price must be >= 0" }
+        require(value = discountPercent in 0..100) { "Discount must be 0..100" }
+        require(value = taxPercent in 0..30) { "Tax must be 0..30" }
+
+        val priceAfterDiscount = basePrice * (1 - discountPercent / 100.0)
+        val taxAmount = priceAfterDiscount * (taxPercent / 100.0)
+
+        val finalPrice = priceAfterDiscount + taxAmount
+
+        return finalPrice.coerceAtLeast(minimumValue = 0.0)
+    }
+
+    @Test
+    fun base() {
+        val result = calculateFinalPrice(1000.0, 10, 20)
+        assertEquals(1080.0, result)
+    }
+
+    @Test
+    fun nodiscount() {
+        val result = calculateFinalPrice(1000.0, 0, 20)
+        assertEquals(1200.0, result)
+    }
+
+    @Test
+    fun notax() {
+        val result = calculateFinalPrice(1000.0, 10, 0)
+        assertEquals(900.0, result)
+    }
+
+    @Test
+    fun onlybase() {
+        val result = calculateFinalPrice(1000.0, 0, 0)
+        assertEquals(1000.0, result)
+    }
+
+}
